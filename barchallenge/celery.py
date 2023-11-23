c8b4974f7a13e24a400940e656a33dd69d6401e8
Jorge Herrera <jorge.herrera.tume@gmail.com>
2023-11-23 17:23:23 -0500

    first commit

--- barchallenge/celery.py
+++ barchallenge/celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barchallenge.settings')
 
-app = Celery('your_project_name')
+app = Celery('barchallenge')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
