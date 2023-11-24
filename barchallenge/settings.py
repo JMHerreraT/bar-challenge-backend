17ffacccf5e12a3c8515bc8ed9359c7301cb64a5
Jorge Herrera <jorge.herrera.tume@gmail.com>
2023-11-23 21:35:12 -0500

    fix: cors issue

--- barchallenge/settings.py
+++ barchallenge/settings.py
@@ -43,18 +43,26 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'barchallenge_backend',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'barchallenge.urls'
